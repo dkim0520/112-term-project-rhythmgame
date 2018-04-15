c11b1da7082149f4dadea34d95bb08e99acf7fb4
dkim0520 <davidk2@andrew.cmu.edu>
2018-04-14 20:31:17 -0400

    Added goals for osu data parser

---
+++ osuDataParser.py
@@ -0,0 +1,9 @@
+###Osu! file Data Parser###
+###David Kim, davidk2###
+
+#HitObject format: x, y, time, type, hitSound, extras (default is 0:0:0:0:)
+#                                              sampleSet: additionSet:customIndex:sampleVolume:filename
+#only need y, time, type, and end Y for sliders, 
+#number of times going over slider can determine length of corresponding platform
+
+
